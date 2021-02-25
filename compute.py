4eb4dae208783ddaf5b8f9b7397916fe3c9cb1d9
Toru Tamaki <tamaki.toru@nitech.ac.jp>
2021-02-25 08:26:14 +0000

    add code of pn for a > b and a < b

--- compute.py
+++ compute.py
@@ -12,5 +12,10 @@ def myadd(a, b):
     if b == 0:
         return a
 
+    if a > 0 and b < 0:
+        if a > b:
+            return a + b
+        if a < b:
+            return a + b
 
     return 0
