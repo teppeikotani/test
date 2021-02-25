dcf0bcd0011c9f33d4d9814f1b595900fb92d36f
Toru Tamaki <tamaki.toru@nitech.ac.jp>
2021-02-25 09:14:21 +0000

    add code of mymult

--- compute.py
+++ compute.py
@@ -22,4 +22,23 @@ def myadd(a, b):
 
 
 def mymult(a, b):
+
+    if a > 0 and b > 0:
+        return a * b
+    
+    if a < 0 and b < 0:
+        return a * b
+
+    if a > 0 and b < 0:
+        return a * b
+    
+    if a < 0 and b > 0:
+        return a * b
+
+    if a == 0:
+        return 0
+
+    if b == 0:
+        return 0
+
     return -100
