bdc518a864651b6851e20c4c0de0caa6d961b873
Toru Tamaki <tamaki.toru@nitech.ac.jp>
2021-02-25 07:57:45 +0000

    add test_add_{pp,nn} rename class name

--- test_add.py
+++ test_add.py
@@ -2,11 +2,19 @@ import unittest
 from compute import myadd
 
 
-class TestMyCompute(unittest.TestCase):
+class TestMyAdd(unittest.TestCase):
 
     def test_add(self):
         val = myadd(1, 2)
         self.assertEqual(3, val)
+
+    def test_add_pp(self):
+        val = myadd(2, 6)
+        self.assertTrue(val > 0)
+
+    def test_add_nn(self):
+        val = myadd(-2, -6)
+        self.assertTrue(val < 0)
 
 
 if __name__ == "__main__":
